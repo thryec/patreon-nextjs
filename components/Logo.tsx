dc3162d249d01c1ffb8df519055e8979fc90e78b
thryec <thryec@gmail.com>
2022-07-17 18:12:05 +0200

    updated logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,11 +1,14 @@
-const Spinner = () => {
+const Logo = () => {
   return (
     <div>
-      <svg height="100" width="100">
-        <circle cx="50" cy="50" r="30" stroke="black" strokeWidth="3" />
+      <svg height="120" width="120" fill="none">
+        <circle cx="60" cy="60" r="40" stroke="#292929" strokeWidth="2" />
+        <circle cx="65" cy="66" r="40" stroke="#292929" strokeWidth="2" />
+        <circle cx="60" cy="68" r="40" stroke="#292929" strokeWidth="2" />
+        <circle cx="66" cy="60" r="40" stroke="#292929" strokeWidth="2" />
       </svg>
     </div>
   )
 }
 
-export default Spinner
+export default Logo
