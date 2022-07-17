4c447deddf99a213bbc51318ecf38d9f33343cea
thryec <thryec@gmail.com>
2022-07-17 10:04:22 +0200

    added user profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Profile: NextPage = () => {
+  return (
+    <div className="flex justify-center h-screen bg-gray-100 ">
+      Current User Profile
+    </div>
+  )
+}
+
+export default Profile
