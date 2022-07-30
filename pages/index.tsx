50f3b3cc5cf2be3a3228056f5f5a201f503bf1e0
Chloe Tan <thryec@Chloes-MBP.localdomain>
2022-07-30 17:50:08 +0800

    refactor contract read to wagmi hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,54 +2,62 @@ import type { NextPage } from 'next'
 import { ethers } from 'ethers'
 import CreatorInfo from '../components/CreatorInfo'
 import { KOVAN_TESTNET_ADDRESS, CONTRACT_ABI } from '../constants'
-import Link from 'next/link'
 import Landing from '../components/Landing'
 import { useState, useEffect, useRef } from 'react'
-import { useContract } from 'wagmi'
+import { useContractReads } from 'wagmi'
 
 const Home: NextPage = () => {
   const ref = useRef<HTMLDivElement>(null)
   const [profileData, setProfileData] = useState<any>([])
-  const [isLoaded, setIsLoaded] = useState<boolean>(false)
 
   const provider = new ethers.providers.JsonRpcProvider(
     process.env.KOVAN_RPC_URL
   )
 
-  const contract = useContract({
+  const contract = {
     addressOrName: KOVAN_TESTNET_ADDRESS,
     contractInterface: CONTRACT_ABI,
-    signerOrProvider: provider,
+  }
+
+  const { data, isError, isLoading } = useContractReads({
+    contracts: [
+      {
+        ...contract,
+        functionName: 'getAllProfiles',
+      },
+    ],
+    onSuccess(data) {
+      console.log('Success', data)
+    },
+    onError(error) {
+      console.log('Error', error)
+    },
   })
 
-  const fetchAllProfiles = async () => {
-    const profiles = await contract.getAllProfiles()
-    console.log('all profiles:', profiles)
-    const result: any = []
-    profiles.map(async (el: string) => {
-      const data = await fetch(el)
-      const res = await data.json()
-      result.push(res)
-      setProfileData(result)
-    })
-  }
+  useEffect(() => {
+    if (!!data) {
+      console.log('data: ', data[0])
+      const result: any = []
+      data[0].map(async (el: string) => {
+        const data = await fetch(el)
+        const res = await data.json()
+        result.push(res)
+        setProfileData(result)
+      })
+    }
+  }, [data])
+
+  useEffect(() => {
+    console.log('profile data:', profileData)
+  }, [profileData])
 
   const handleSupporterClick = () => {
     ref.current?.scrollIntoView({ behavior: 'smooth', inline: 'center' })
   }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetchAllProfiles()
-      setIsLoaded(true)
-    }
-    fetchData()
-  }, [])
-
-  const profileCards = profileData.map((el: any) => {
+  const profileCards = profileData.map((el: any, key: any) => {
     return (
       <CreatorInfo
-        key={el.walletAddress}
+        key={key}
         address={el.walletAddress}
         name={el.name}
         description={el.description}
@@ -63,15 +71,13 @@ const Home: NextPage = () => {
       <div className="">
         <Landing handleSupporterClick={handleSupporterClick} />
         <div
-          className="bg-gray-100 h-screen w-screen pt-16 border-t-2 border-slate-200"
+          className="bg-gray-100 h-screen w-screen pt-16 border-t-4 border-slate-200"
           ref={ref}
         >
           <h1 className="text-center text-5xl font-extrabold pb-20">
             Discover Creators.
           </h1>
-          <div className="grid grid-cols-3 gap-10 px-60">
-            {isLoaded && profileCards}
-          </div>
+          <div className="grid grid-cols-3 gap-10 px-60">{profileCards}</div>
         </div>
       </div>
     </div>
