de5b26643c541d7c48d495d8c70feb0cee8b4ccd
thryec <thryec@gmail.com>
2022-07-31 17:31:42 +0800

    improved waitlist and footer ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,27 @@
+import twitter from '../public/twitter.svg'
+import github from '../public/github.svg'
+import Image from 'next/image'
+
 const Footer = () => {
   return (
-    <div>
-      <div className="text-center">Connect</div>
+    <div className="text-center pb-20 border-t-4 border-slate-200">
+      <h1 className="text-6xl font-extrabold py-10">Connect</h1>
+      <div className="space-x-10">
+        <a
+          href="https://twitter.com/circleofliife"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image src={twitter} height={35} width={35} alt="twitter logo" />
+        </a>
+        <a
+          href="https://twitter.com/circleofliife"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image src={github} height={35} width={35} alt="github logo" />
+        </a>
+      </div>
     </div>
   )
 }
--- components/WaitlistForm.tsx
+++ components/WaitlistForm.tsx
@@ -18,28 +18,36 @@ const WaitlistForm = () => {
   }
 
   return (
-    <div className="text-center h-1/2">
+    <div className="text-center h-1/2 text-2xl">
       <form onSubmit={submitForm}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <div className="space-y-8">
+          <div className="space-x-4">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              className="px-4 py-2"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="space-x-4">
+            <label htmlFor="type">How will you mainly use this platform?</label>
+            <select
+              onChange={(e) => setType(e.target.value)}
+              className="px-4 py-2"
+            >
+              <option selected value="creator">
+                creator
+              </option>
+              <option value="contributor">contributor</option>
+              <option value="both">both</option>
+            </select>
+          </div>
         </div>
-        <div>
-          <label htmlFor="type">How will you be using this platform?</label>
-          <select onChange={(e) => setType(e.target.value)}>
-            <option value="creator">creator</option>
-            <option selected value="contributor">
-              contributor
-            </option>
-            <option value="both">both</option>
-          </select>
-        </div>
-
         <button
           type="submit"
-          className="bg-violet-500 py-2 px-3 rounded-lg text-xl text-white font-semibold"
+          className="bg-violet-500 py-2 px-3 mt-20 rounded-lg text-2xl text-white font-semibold"
         >
-          Join
+          Submit
         </button>
       </form>
     </div>
