1e3fe5b55007ff982277aa8bc2efc0a659d3f9ad
thryec <thryec@gmail.com>
2022-08-04 15:54:56 +0800

    shorten desc in creator card

--- components/CreatorInfo.tsx
+++ components/CreatorInfo.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { useState } from 'react'
 import ContributeModal from './ContributeModal'
-import { shortenAddress } from '../helpers'
+import { shortenAddress, shortenDescription } from '../helpers'
 import Image from 'next/image'
 
 interface CreatorProps {
@@ -14,7 +14,7 @@ interface CreatorProps {
 const CreatorInfo = (creator: CreatorProps) => {
   return (
     <Link href={'/creator/' + creator.address} passHref>
-      <div className="border-4 rounded-lg border-slate-200 p-5 space-y-5 cursor-pointer">
+      <div className="border-4 rounded-lg border-violet-300 p-7 space-y-5 cursor-pointer h-80">
         <div className="flex place-content-between items-center">
           <div className="flex items-center space-x-6">
             <Image
@@ -26,11 +26,11 @@ const CreatorInfo = (creator: CreatorProps) => {
             />
             <h1 className="text-2xl font-bold inline"> {creator.name}</h1>
           </div>
-          <span className=" text-slate-500">
+          <span className="text-slate-500">
             {shortenAddress(creator.address)}
           </span>
         </div>
-        <div className="text-xl">{creator.description}</div>
+        <div className="text-lg">{shortenDescription(creator.description)}</div>
       </div>
     </Link>
   )
--- helpers.ts
+++ helpers.ts
@@ -2,6 +2,14 @@ export const shortenAddress = (str: any) => {
   return str.substring(0, 4) + '...' + str.substring(str.length - 4)
 }
 
+export const shortenDescription = (str: any) => {
+  if (str.length < 180) {
+    return str
+  } else {
+    return str.substring(0, 180) + '.....'
+  }
+}
+
 export const validateAddress = (input: string) => {
   const prefix = input.slice(0, 2)
   if (input.length === 42 && prefix === '0x') {
